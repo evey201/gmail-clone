c11adf1ff6a8b727a7cf82a908e0823e09d4f660
evey201 <evesho.alabi@gmail.com>
2021-01-02 18:44:46 +0100

    worked on the header section of the page

---
+++ src/components/header/Header.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './header.css';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import AppsIcon from '@material-ui/icons/Apps';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import { Avatar, IconButton } from '@material-ui/core';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="header__left">
+                <IconButton>
+                    <MenuIcon />
+                </IconButton>
+                <img src="https://i.pinimg.com/originals/ae/47/fa/ae47fa9a8fd263aa364018517020552d.png" alt="" />
+            </div>
+
+            <div className="header__middle">
+                <SearchIcon />
+                <input placeholder="Search mail" type="text" />
+                <ArrowDropDownIcon className="header__inputCaret" />
+
+            </div>
+
+            <div className="header__right">
+                <IconButton>
+                    <AppsIcon />
+                </IconButton>
+                <IconButton>
+                    <NotificationsIcon />
+                </IconButton>
+                <Avatar />
+            </div>
+        </div>
+
+    );
+}
+
+export default Header;
